48415315f69c8c2d748422084ae1bb713dbae46e
Ifthikar Ali <ifthicaralikhan@gmail.com>
2024-07-14 12:13:43 -0500

    Added middleware and front end validation

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,17 @@
+// import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+// import {Box} from "@mui/material";
+// import {CssBaseline,ThemeProvider} from "@mui/material";
+// import { createTheme } from "@mui/material/styles";
+// import { useMemo } from "react";
+// import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
+export default function Home() {
+  return (
+    <p>
+      <UserButton afterSignOutUrl="/"/> 
+      Dashboard page
+    </p>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { request } from "http";
+
+
+const isProtectedRoute = createRouteMatcher([
+    '/',
+  ]);
+export default clerkMiddleware((auth, request)=>{
+if(isProtectedRoute(request)){
+    auth().protect();
+}
+return NextResponse.next();
+});
+
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
